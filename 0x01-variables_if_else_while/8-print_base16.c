b46fd69c11dae117b949140de315d67d3d0fb28f
KidusGR <kidmen10@gmail.com>
2022-10-14 06:03:17 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints numbers from 0 to 9
+ * Return: 0
+ */
+
+int main(void)
+{
+	long long int n = 9876543210;
+	char ch[8] = "abcdef";
+	int i = 0;
+	int j = 0;
+
+	while (i < 10)
+	{
+		putchar((n%10) + '0');
+		n = n/10;
+		i++;
+	}
+	while (j < 6)
+	{
+		putchar(ch[j]);
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
+
