ecd6c637554596b58a03aecf7c1a7e76917e9f5b
KidusGR <kidmen10@gmail.com>
2023-01-16 15:31:29 +0300

    0x18. C - Dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/nrandom.so https://raw.github.com/kidusgr/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -O /tmp/win.so https://raw.github.com/kidusgr/alx-low_level_programming/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
